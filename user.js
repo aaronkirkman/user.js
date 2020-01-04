2c5c014f73d7cbb8d978763c4987c8a43c6050bb
Aaron Kirkman <kirkman.aaron.w+github@gmail.com>
2020-01-04 15:08:18 -0600

    disable warning when opening about:config

--- user.js
+++ user.js
@@ -1,3 +1,7 @@
+// SETTING: Disable warning when opening about:config
+user_pref("general.warnOnAboutConfig", false);
+
+
 // SETTING: Disable User Timing API, which stores high-resolution (milliseconds to microseconds) timestamps
 // https://trac.torproject.org/projects/tor/ticket/16336
 // https://www.w3.org/TR/2013/REC-user-timing-20131212/#privacy-security
