b3a17b95e35c3d21e9665b7f5d76d003d17c8d90
Aaron Kirkman <kirkman.aaron.w+github@gmail.com>
2020-01-04 15:01:54 -0600

    whitespace fixes

--- user.js
+++ user.js
@@ -3,6 +3,7 @@
 // https://www.w3.org/TR/2013/REC-user-timing-20131212/#privacy-security
 user_pref("dom.enable_user_timing",	false);
 
+
 // SETTING: Disable DOM Timing API
 // https://wiki.mozilla.org/Security/Reviews/Firefox/NavigationTimingAPI
 // https://www.w3.org/TR/navigation-timing/#privacy
@@ -17,6 +18,7 @@ user_pref("geo.enabled", false);
 // SETTING: When set to false, this setting prevents invalid URLs entered into the address bar from being submitted to the default search engine.
 // http://kb.mozillazine.org/Keyword.enabled
 user_pref("keyword.enabled", true);
+
 
 // SETTING: Enable Firefox's anti-fingerprinting mode ("resist fingerprinting" or RFP) (Tor Uplift project)
 // https://wiki.mozilla.org/Security/Fingerprinting
