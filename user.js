cb42d8d399420199b64ac849d30068b323cd4837
Aaron Kirkman <kirkman.aaron.w+github@gmail.com>
2020-01-04 15:23:47 -0600

    disable telemetry

--- user.js
+++ user.js
@@ -34,3 +34,49 @@ user_pref("keyword.enabled", true);
 //		user_pref("general.platform.override", "Win32");
 //		user_pref("general.oscpu.override", "Windows NT 6.1");
 user_pref("privacy.resistFingerprinting", true);
+
+
+// SETTING: Disable telemetry
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("experiments.supported", false);
+user_pref("experiments.enabled", false);
+user_pref("experiments.manifest.uri", "");
+
+
+// SETTING: Disallow Necko to do A/B testing
+// https://trac.torproject.org/projects/tor/ticket/13170
+user_pref("network.allow-experiments", false);
+
+
+// SETTING: Disable sending Firefox crash reports to Mozilla servers
+// https://wiki.mozilla.org/Breakpad
+// http://kb.mozillazine.org/Breakpad
+// https://dxr.mozilla.org/mozilla-central/source/toolkit/crashreporter
+// https://bugzilla.mozilla.org/show_bug.cgi?id=411490
+// See about:crashes for a list of submitted crash reports
+user_pref("breakpad.reportURL", "");
+
+
+// SETTING: Disable sending reports of tab crashes to Mozilla (about:tabcrashed) and don't nag user about unsent crash reports
+// https://hg.mozilla.org/mozilla-central/file/tip/browser/app/profile/firefox.js
+user_pref("browser.tabs.crashReporting.sendReport", false);
+user_pref("browser.crashReports.unsubmittedCheck.enabled", false);
+
+
+// SETTING: Disable FlyWeb (discovery of LAN/proximity IoT devices that expose a Web interface)
+// https://wiki.mozilla.org/FlyWeb
+// https://wiki.mozilla.org/FlyWeb/Security_scenarios
+// http://www.ghacks.net/2016/07/26/firefox-flyweb
+user_pref("dom.flyweb.enabled", false);
+
+
+// SETTING: Disable the UITour backend
+// https://trac.torproject.org/projects/tor/ticket/19047#comment:3
+user_pref("browser.uitour.enabled", false);
+
+
+// SETTING: Disable beacon, which sends additional analytics data
+// https://developer.mozilla.org/en-US/docs/Web/API/Navigator/sendBeacon
+user_pref("beacon.enabled", false);
