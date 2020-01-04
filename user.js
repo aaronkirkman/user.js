3a871f76d09c1a1effcabd1e79dc51a86fd7c827
Aaron Kirkman <kirkman.aaron.w+github@gmail.com>
2020-01-04 14:37:13 -0600

    enable keyword searches

--- user.js
+++ user.js
@@ -2,3 +2,8 @@
 // https://trac.torproject.org/projects/tor/ticket/16336
 // https://www.w3.org/TR/2013/REC-user-timing-20131212/#privacy-security
 user_pref("dom.enable_user_timing",	false);
+
+
+// SETTING: When set to false, this setting prevents invalid URLs entered into the address bar from being submitted to the default search engine.
+// http://kb.mozillazine.org/Keyword.enabled
+user_pref("keyword.enabled", true);
