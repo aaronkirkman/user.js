a35f807651249c932d744613e539c8887ec555d1
Aaron Kirkman <kirkman.aaron.w+github@gmail.com>
2020-01-05 15:33:48 -0600

    Add encryption preferences.
    Attribution: https://github.com/pyllyukko/user.js

--- user.js
+++ user.js
@@ -150,3 +150,103 @@ user_pref("extensions.pocket.enabled", false);
 // http://kb.mozillazine.org/Network.prefetch-next
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Link_prefetching_FAQ#Privacy_implications
 user_pref("network.prefetch-next", false);
+
+
+
+
+/******************************************************************************
+ * SECTION: Cipher suites                                                     *
+ ******************************************************************************/
+
+// SETTING: Disable null ciphers
+user_pref("security.ssl3.rsa_null_sha", false);
+user_pref("security.ssl3.rsa_null_md5", false);
+user_pref("security.ssl3.ecdhe_rsa_null_sha", false);
+user_pref("security.ssl3.ecdhe_ecdsa_null_sha", false);
+user_pref("security.ssl3.ecdh_rsa_null_sha", false);
+user_pref("security.ssl3.ecdh_ecdsa_null_sha", false);
+
+// SETTING: Disable SEED cipher
+// https://en.wikipedia.org/wiki/SEED
+user_pref("security.ssl3.rsa_seed_sha", false);
+
+// SETTING: Disable 40/56/128-bit ciphers
+// 40-bit ciphers
+user_pref("security.ssl3.rsa_rc4_40_md5", false);
+user_pref("security.ssl3.rsa_rc2_40_md5", false);
+// 56-bit ciphers
+user_pref("security.ssl3.rsa_1024_rc4_56_sha", false);
+// 128-bit ciphers
+user_pref("security.ssl3.rsa_camellia_128_sha", false);
+user_pref("security.ssl3.ecdhe_rsa_aes_128_sha", false);
+user_pref("security.ssl3.ecdhe_ecdsa_aes_128_sha", false);
+user_pref("security.ssl3.ecdh_rsa_aes_128_sha", false);
+user_pref("security.ssl3.ecdh_ecdsa_aes_128_sha", false);
+user_pref("security.ssl3.dhe_rsa_camellia_128_sha", false);
+user_pref("security.ssl3.dhe_rsa_aes_128_sha", false);
+
+// SETTING: Disable RC4
+// https://developer.mozilla.org/en-US/Firefox/Releases/38#Security
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1138882
+// https://rc4.io/
+// https://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2013-2566
+user_pref("security.ssl3.ecdh_ecdsa_rc4_128_sha", false);
+user_pref("security.ssl3.ecdh_rsa_rc4_128_sha", false);
+user_pref("security.ssl3.ecdhe_ecdsa_rc4_128_sha", false);
+user_pref("security.ssl3.ecdhe_rsa_rc4_128_sha", false);
+user_pref("security.ssl3.rsa_rc4_128_md5", false);
+user_pref("security.ssl3.rsa_rc4_128_sha", false);
+user_pref("security.tls.unrestricted_rc4_fallback", false);
+
+// SETTING: Disable 3DES (effective key size is < 128)
+// https://en.wikipedia.org/wiki/3des#Security
+// http://en.citizendium.org/wiki/Meet-in-the-middle_attack
+// http://www-archive.mozilla.org/projects/security/pki/nss/ssl/fips-ssl-ciphersuites.html
+user_pref("security.ssl3.dhe_dss_des_ede3_sha", false);
+user_pref("security.ssl3.dhe_rsa_des_ede3_sha", false);
+user_pref("security.ssl3.ecdh_ecdsa_des_ede3_sha", false);
+user_pref("security.ssl3.ecdh_rsa_des_ede3_sha", false);
+user_pref("security.ssl3.ecdhe_ecdsa_des_ede3_sha", false);
+user_pref("security.ssl3.ecdhe_rsa_des_ede3_sha", false);
+user_pref("security.ssl3.rsa_des_ede3_sha", false);
+user_pref("security.ssl3.rsa_fips_des_ede3_sha", false);
+
+// SETTING: Disable ciphers with ECDH (non-ephemeral)
+user_pref("security.ssl3.ecdh_rsa_aes_256_sha", false);
+user_pref("security.ssl3.ecdh_ecdsa_aes_256_sha", false);
+
+// SETTING: Disable 256 bits ciphers without PFS
+user_pref("security.ssl3.rsa_camellia_256_sha", false);
+
+// SETTING: Enable ciphers with ECDHE and key size > 128bits
+user_pref("security.ssl3.ecdhe_rsa_aes_256_sha", true); // 0xc014
+user_pref("security.ssl3.ecdhe_ecdsa_aes_256_sha", true); // 0xc00a
+
+// SETTING: Enable GCM ciphers (TLSv1.2 only)
+// https://en.wikipedia.org/wiki/Galois/Counter_Mode
+user_pref("security.ssl3.ecdhe_ecdsa_aes_128_gcm_sha256", true); // 0xc02b
+user_pref("security.ssl3.ecdhe_rsa_aes_128_gcm_sha256", true); // 0xc02f
+
+// SETTING: Enable ChaCha20 and Poly1305 (Firefox >= 47)
+// https://www.mozilla.org/en-US/firefox/47.0/releasenotes/
+// https://tools.ietf.org/html/rfc7905
+// https://bugzilla.mozilla.org/show_bug.cgi?id=917571
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1247860
+// https://cr.yp.to/chacha.html
+user_pref("security.ssl3.ecdhe_ecdsa_chacha20_poly1305_sha256", true);
+user_pref("security.ssl3.ecdhe_rsa_chacha20_poly1305_sha256", true);
+
+// SETTING: Disable ciphers susceptible to the logjam attack
+// https://weakdh.org/
+user_pref("security.ssl3.dhe_rsa_camellia_256_sha", false);
+user_pref("security.ssl3.dhe_rsa_aes_256_sha", false);
+
+// SETTING: Disable ciphers with DSA (max 1024 bits)
+user_pref("security.ssl3.dhe_dss_aes_128_sha", false);
+user_pref("security.ssl3.dhe_dss_aes_256_sha", false);
+user_pref("security.ssl3.dhe_dss_camellia_128_sha", false);
+user_pref("security.ssl3.dhe_dss_camellia_256_sha", false);
+
+// SETTING: Fallbacks due compatibility reasons
+user_pref("security.ssl3.rsa_aes_256_sha", true); // 0x35
+user_pref("security.ssl3.rsa_aes_128_sha", true); // 0x2f
