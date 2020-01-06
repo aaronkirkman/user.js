af924ab75865eeccca963c3126f9ed2985e11f64
Aaron Kirkman <kirkman.aaron.w+github@gmail.com>
2020-01-06 12:28:53 -0600

    disable built-in PDF viewer (pdf.js)

--- user.js
+++ user.js
@@ -188,6 +188,13 @@ user_pref("browser.safebrowsing.downloads.remote.enabled", false);
 user_pref("extensions.shield-recipe-client.enabled", false);
 user_pref("app.shield.optoutstudies.enabled", false);
 
+
+// SETTING: Disable the built-in PDF viewer
+// https://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2015-2743
+// https://blog.mozilla.org/security/2015/08/06/firefox-exploit-found-in-the-wild/
+// https://www.mozilla.org/en-US/security/advisories/mfsa2015-69/
+user_pref("pdfjs.disabled", true);
+
 /******************************************************************************
  * SECTION: Automatic connections                                             *
  ******************************************************************************/
