9c9c1ec867c10205d00e11fe305cdacd095b5699
Aaron Kirkman <kirkman.aaron.w+github@gmail.com>
2020-01-06 12:49:50 -0600

    disable Java plugin

--- user.js
+++ user.js
@@ -29,6 +29,10 @@ user_pref("browser.safebrowsing.blockedURIs.enabled", true);
 user_pref("shumway.disabled", true);
 
 
+// SETTING: Disable Java NPAPI plugin
+user_pref("plugin.state.java", 0);
+
+
 // SETTING: Disable User Timing API, which stores high-resolution (milliseconds to microseconds) timestamps
 // https://trac.torproject.org/projects/tor/ticket/16336
 // https://www.w3.org/TR/2013/REC-user-timing-20131212/#privacy-security
